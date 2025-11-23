b790e520a5d2f8c267c701118808abf0ede0bf2c
dave199323 <sidunadave@gmail.com>
2025-11-23 17:45:07 +0300

    whywhy

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,11 @@ import {cacheLife} from "next/cache";
 
 
 
-
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 const Page = async () => {
     'use cache';
-    cacheLife('hours');
+    cacheLife('hours')
     const response = await fetch(`${BASE_URL}/api/events`, {
         cache: "no-store",
         next: { revalidate: 0 },
