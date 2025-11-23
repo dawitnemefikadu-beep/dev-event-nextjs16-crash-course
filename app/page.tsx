9d41dd9c689323e9919bc3e84d297f3a0822300e
dave199323 <sidunadave@gmail.com>
2025-11-23 13:42:36 +0300

    html problem

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,10 @@ import {IEvent} from "@/database";
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 const Page = async() => {
-    const response=await fetch(`${BASE_URL}/api/events`);
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL ? process.env.NEXT_PUBLIC_BASE_URL : ''}/api/events`, {
+        cache: 'no-store'
+    });
+
     const {events}=await response.json();
     return (
         <section>
