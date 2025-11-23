a9368943c184d355d42b5a77764d5147e8fe19cb
dave199323 <sidunadave@gmail.com>
2025-11-23 17:42:56 +0300

    problem

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,13 @@ import {IEvent} from "@/database";
 import {cacheLife} from "next/cache";
 
 
-'use cache';
-cacheLife('hours')
+
+
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 const Page = async () => {
-
+    'use cache';
+    cacheLife('hours');
     const response = await fetch(`${BASE_URL}/api/events`, {
         cache: "no-store",
         next: { revalidate: 0 },
